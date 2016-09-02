40d170fb6c23f59fb48f42f1669ccc830a79dbec
Nikola Kostadinov <nikolakk@gmail.com>
2016-09-02 11:43:38 +0300

    set utf8 encoding of tables

--- migrations/m151031_160731_taxonomy.php
+++ migrations/m151031_160731_taxonomy.php
@@ -6,6 +6,15 @@ use yii\db\Migration;
 
 class m151031_160731_taxonomy extends Migration
 {
+    public function getTableOptions()
+    {
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            return 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        } else
+            throw new Exception('Unsupported database.');
+    }
+
     public function up()
     {
         $this->createTable(TaxonomyDef::tableName(), [
@@ -17,7 +26,7 @@ class m151031_160731_taxonomy extends Migration
             'data_table' => $this->string()->notNull(),
             'ref_table' => $this->string()->notNull(),
             'migration' => $this->string()
-        ]);
+        ], $this->getTableOptions());
 
         $this->createTable(TaxonomyTerms::tableName(), [
             'id' => $this->bigPrimaryKey(),
@@ -25,7 +34,7 @@ class m151031_160731_taxonomy extends Migration
             'term' => $this->string(),
             'total_count' => $this->bigInteger()->defaultValue(0),
             'parent_id' => $this->bigInteger(),
-        ]);
+        ], $this->getTableOptions());
 
         if ($this->db->driverName === 'mysql') {
             $this->addForeignKey('fk_TaxonomyTerm_Taxonomy', TaxonomyTerms::tableName(), 'taxonomy_id',
--- migrations/template/tag.php
+++ migrations/template/tag.php
@@ -14,6 +14,15 @@ use yii\db\Migration;
 
 class <?= $data['migration'] ?> extends Migration
 {
+    public function getTableOptions()
+    {
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            return 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        } else
+            throw new Exception('Unsupported database.');
+    }
+
     public function up()
     {
         $this->insert(TaxonomyDef::tableName(),
@@ -24,7 +33,7 @@ class <?= $data['migration'] ?> extends Migration
             'id' => $this->primaryKey(),
             'object_id' => $this->integer()->notNull(),
             'term_id' => $this->bigInteger()->notNull(),
-        ]);
+        ], $this->getTableOptions());
 
         $this->addForeignKey('fk_<?= $data['data_table'] ?>_<?= $data['ref_table'] ?>', '<?= $data['data_table'] ?>', 'object_id', '<?= $data['ref_table'] ?>', 'id', 'CASCADE');
         $this->addForeignKey('fk_<?= $data['data_table'] ?>_' . TaxonomyTerms::tableName(), '<?= $data['data_table'] ?>', 'term_id', TaxonomyTerms::tableName(), 'id', 'CASCADE');
